487ff34adc4e551a83377049e361831665306b53
Aaron <aaron.ciaghi@gmail.com>
2013-07-14 12:13:37 +0200

    updated setup script

--- utils/ssror.sh
+++ utils/ssror.sh
@@ -2,8 +2,14 @@
 sudo sh <<SCRIPT
 	echo 'Upgrading system'
 	echo '================'
+	# install heroku's release key for package verification
+	wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
+	# add heroku repository to apt
+	echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
+	# update your sources
+	apt-get update
 	apt-get -y upgrade
-	apt-get -y install curl git-core ia32-libs ia32-libs-multiarch nodejs nodejs-dev chromium-browser
+	apt-get -y install curl git-core ia32-libs ia32-libs-multiarch nodejs nodejs-dev heroku-toolbelt chromium-browser
 
 	echo 'Installing RVM'
 	echo '=============='
@@ -13,27 +19,12 @@ sudo sh <<SCRIPT
 	echo 'Installing Ruby 1.9.3'
 	echo '====================='
 	rvm install 1.9.3
-
-	echo 'Installing Rails'
-	echo '================'
-	gem install rails -v 3.2.13
-
-	echo 'Installing Redcar'
-	echo '================='
-	gem install redcar
-
-	echo 'Installing Heroku'
-	echo '================='
-	# add heroku repository to apt
-	echo "deb http://toolbelt.heroku.com/ubuntu ./" > /etc/apt/sources.list.d/heroku.list
-
-	# install heroku's release key for package verification
-	wget -O- https://toolbelt.heroku.com/apt/release.key | apt-key add -
-
-	# update your sources
-	apt-get update
-
-	# install the toolbelt
-	apt-get install -y heroku-toolbelt
 SCRIPT
 
+echo 'Installing Rails'
+echo '================'
+gem install rails -v 3.2.13
+
+echo 'Installing Redcar'
+echo '================='
+gem install redcar
